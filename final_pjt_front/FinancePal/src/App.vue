40c36d7521562f3807d96c6188af5d0d5ae46940
jiwon <leeji7031@gmail.com>
2023-11-16 16:48:57 +0900

    feat: 프론트 기본 구조 완성

--- final_pjt_front/FinancePal/src/App.vue
+++ final_pjt_front/FinancePal/src/App.vue
@@ -1,85 +1,34 @@
-<script setup>
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
-    </div>
-  </header>
-
-  <RouterView />
+  <div>
+    <nav>
+      <RouterLink :to="{ name: 'main'}">메인</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'product'}">상품비교</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'exchange'}">환율</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'map'}">주위 은행</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'login'}">로그인</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'signup'}">회원가입</RouterLink>
+      <span> | </span>
+      <RouterLink :to="{ name: 'profile', params: { username: accStore.user.username }}">프로필</RouterLink>
+      <span> | </span>
+    </nav>
+    <RouterView />
+  </div>
 </template>
 
+<script setup>
+import { RouterLink, RouterView } from 'vue-router'
+import { accountStore } from '@/stores/accountStore'
+
+const accStore = accountStore()
+
+
+</script>
+
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
-}
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
-}
 </style>
--- final_pjt_front/FinancePal/src/router/index.js
+++ final_pjt_front/FinancePal/src/router/index.js
@@ -1,22 +1,56 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import MainView from '@/views/MainView.vue'
+import LoginView from '@/views/LoginView.vue'
+import SignupView from '@/views/SignupView.vue'
+import ProfileView from '@/views/ProfileView.vue'
+import ProductView from '@/views/ProductView.vue'
+import ProductDetailView from '@/views/ProductDetailView.vue'
+import ExchangeView from '@/views/ExchangeView.vue'
+import MapView from '@/views/MapView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'home',
-      component: HomeView
+      name: 'main',
+      component: MainView
     },
     {
-      path: '/about',
-      name: 'about',
-      // route level code-splitting
-      // this generates a separate chunk (About.[hash].js) for this route
-      // which is lazy-loaded when the route is visited.
-      component: () => import('../views/AboutView.vue')
-    }
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: SignupView
+    },
+    {
+      path: '/profile/:username',
+      name: 'profile',
+      component: ProfileView
+    },
+    {
+      path: '/product',
+      name: 'product',
+      component: ProductView
+    },
+    {
+      path: '/product/:cd',
+      name: 'productDetail',
+      component: ProductDetailView
+    },
+    {
+      path: '/exchange',
+      name: 'exchange',
+      component: ExchangeView
+    },
+    {
+      path: '/map',
+      name: 'map',
+      component: MapView
+    },
   ]
 })
 
