644ddfb0eb3fa1847950e5ea6517f9becb2b6c68
jiwon <leeji7031@gmail.com>
2023-11-17 15:33:27 +0900

    feat: front/product deposit 미완

--- final_pjt_front/FinancePal/src/views/ProductView.vue
+++ final_pjt_front/FinancePal/src/views/ProductView.vue
@@ -1,11 +1,15 @@
 <template>
   <div>
     <h1>ProductView</h1>
+    <ProductDeposits />
   </div>
 </template>
 
 <script setup>
-
+import { RouterLink, RouterView } from 'vue-router'
+import ProductDeposits from '@/components/ProductDeposits.vue'
+import ProductSavingS from '@/components/ProductSavingsS.vue'
+import ProductSavingF from '@/components/ProductSavingsF.vue'
 </script>
 
 <style scoped>
