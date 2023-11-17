75da5fdd2bec42d1b295c2c2bccdad04690b05dd
suminjeff <suminjeff@gmail.com>
2023-11-17 10:47:16 +0900

    feat : products model 정의

--- final_pjt_back/products/models.py
+++ final_pjt_back/products/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Deposit(models.Model):
+    pass
+
+
+class DepositOption(models.Model):
+    pass
+
+
+class Saving(models.Model):
+    pass
+
+
+class SavingOption(models.Model):
+    pass
+
+
+class ExchangeRate(models.Model):
+    pass
+
+
+class Article(models.Model):
+    pass
+
+
+class Comment(models.Model):
+    pass
