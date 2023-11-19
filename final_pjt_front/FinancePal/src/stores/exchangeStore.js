eb783fec434d934b18751648830df5a311910509
Jiwon <leeji7031@gmail.com>
2023-11-20 04:04:03 +0900

    feat: front/exchange

--- final_pjt_front/FinancePal/src/stores/exchangeStore.js
+++ final_pjt_front/FinancePal/src/stores/exchangeStore.js
@@ -3,7 +3,17 @@ import { ref, computed } from 'vue'
 import { useRouter } from 'vue-router'
 import axios from 'axios'
 
-const exchangeStore = defineStore('exchange', () => {
-  const exchageList = ref(null)
+export const exchangeStore = defineStore('exchange', () => {
+  const exchangeList = ref(null)
+  const updateExchange = function () {
+    axios({
+      method: 'get',
+      url: 'http://127.0.0.1:8000/exchange_rate/all/'
+    })
+      .then((res) => {
+        exchangeList.value = res.data
+      })
+  }
 
+  return { exchangeList, updateExchange }
 })
--- final_pjt_front/FinancePal/src/views/ExchangeView.vue
+++ final_pjt_front/FinancePal/src/views/ExchangeView.vue
@@ -1,10 +1,46 @@
 <template>
   <div>
     <h1>ExchangeView</h1>
+    <div>
+      <span>대한민국</span>
+      <input type="number" v-model="korInput" @input="calOther">
+    </div>
+    <div>
+      <select name="other" id="other" v-model="country" @change="calOther">
+        <template v-for="exchange in store.exchangeList">
+          <option :value="exchange.cur_nm">{{ exchange.cur_nm }}</option>
+        </template>
+      </select>
+      <input type="number" v-model="otherInput" @input="calKor">
+    </div>
   </div>
 </template>
 
 <script setup>
+import { ref, computed } from 'vue'
+import { exchangeStore } from '@/stores/exchangeStore'
+import { onMounted } from 'vue'
+
+const store = exchangeStore()
+const country = ref('미국 달러')
+const exchangeRate = computed(() => {
+  const idx = store.exchangeList.findIndex((ele) => ele.cur_nm === country.value)
+  return store.exchangeList[idx].deal_bas_r
+})
+const korInput = ref(0)
+const otherInput = ref(0)
+
+const calOther = function() {
+  otherInput.value = korInput.value / exchangeRate.value
+}
+const calKor = function() {
+  korInput.value = otherInput.value * exchangeRate.value
+}
+
+
+onMounted(() => {
+  store.updateExchange()
+})
 
 </script>
 
