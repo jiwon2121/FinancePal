8c97a9b48a61485a96ea65a8e8c9f0a9bdfe2368
suminjeff <suminjeff@gmail.com>
2023-11-20 09:45:23 +0900

    feat : articles 모델 게시판종류 필드 추가, exchange_rate (100) 있는 화폐 데이터 view함수 수정

--- final_pjt_back/articles/models.py
+++ final_pjt_back/articles/models.py
@@ -6,6 +6,7 @@ class Article(models.Model):
     user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
     title = models.CharField(max_length=255)
     content = models.TextField()
+    board_type = models.CharField(max_length=50) # free: 자유게시판, notice: 공지사항, qna: Q&A
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
