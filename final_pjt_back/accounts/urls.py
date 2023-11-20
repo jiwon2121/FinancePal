b8f82ff470dd416ffc5a638d358844fcc87959c2
suminjeff <suminjeff@gmail.com>
2023-11-20 16:23:47 +0900

    feat : 추천 알고리즘 초본

--- final_pjt_back/accounts/urls.py
+++ final_pjt_back/accounts/urls.py
@@ -4,5 +4,7 @@ from . import views
 app_name = 'profile'
 urlpatterns = [
     path('<str:username>/', views.profile),
-    path('save/dummy_user_deposit_saving/', views.dummy_user_deposit_saving)
+    path('save/dummy_user_deposit_saving/', views.dummy_user_deposit_saving),
+    path('recommend_by_profile/<str:username>', views.recommend_by_profile),
+    path('recommend_by_portfolio/<str:username>', views.recommend_by_portfolio),
 ]
