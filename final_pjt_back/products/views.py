175d14e38fb6f30ee9d25c229b80d5243f8d3a38
suminjeff <suminjeff@gmail.com>
2023-11-20 17:38:26 +0900

    feat : products api 요청 시 업데이트

--- final_pjt_back/products/views.py
+++ final_pjt_back/products/views.py
@@ -19,12 +19,20 @@ def save_deposit_products(request):
     url = f'http://finlife.fss.or.kr/finlifeapi/depositProductsSearch.json?auth={api_key}&topFinGrpNo=020000&pageNo=1'
     response = requests.get(url).json()
     for obj in response.get('result').get("baseList"):
-        products_serializer = DepositSerializer(data=obj)
+        if Deposit.objects.filter(pk=obj.get('fin_prdt_cd')).exists():
+            instance = Deposit.objects.get(pk=obj.get('fin_prdt_cd'))
+            products_serializer = DepositSerializer(instance=instance, data=obj, partial=True)
+        else:
+            products_serializer = DepositSerializer(data=obj)
         if products_serializer.is_valid(raise_exception=True):
             products_serializer.save()
     for obj in response.get('result').get('optionList'):
-        product = Deposit.objects.get(fin_prdt_cd=obj.get('fin_prdt_cd'))
-        options_serializer = DepositOptionSerializer(data=obj)
+        product = Deposit.objects.get(pk=obj.get('fin_prdt_cd'))
+        if DepositOption.objects.filter(product=obj.get('fin_prdt_cd'), save_trm=obj.get('save_trm')).exists():
+            instance = DepositOption.objects.get(product=obj.get('fin_prdt_cd'), save_trm=obj.get('save_trm'))
+            options_serializer = DepositOptionSerializer(instance=instance, data=obj, partial=True)
+        else:
+            options_serializer = DepositOptionSerializer(data=obj)
         if options_serializer.is_valid(raise_exception=True):
             options_serializer.save(product=product)
     return Response({'message':'okay'})
@@ -55,12 +63,20 @@ def save_saving_products(request):
     url = f'http://finlife.fss.or.kr/finlifeapi/savingProductsSearch.json?auth={api_key}&topFinGrpNo=020000&pageNo=1'
     response = requests.get(url).json()
     for obj in response.get('result').get("baseList"):
-        products_serializer = SavingSerializer(data=obj)
+        if Saving.objects.filter(pk=obj.get('fin_prdt_cd')).exists():
+            instance = Saving.objects.get(pk=obj.get('fin_prdt_cd'))
+            products_serializer = SavingSerializer(instance=instance, data=obj, partial=True)
+        else:
+            products_serializer = SavingSerializer(data=obj)
         if products_serializer.is_valid(raise_exception=True):
             products_serializer.save()
     for obj in response.get('result').get('optionList'):
-        product = Saving.objects.get(fin_prdt_cd=obj.get('fin_prdt_cd'))
-        options_serializer = SavingOptionSerializer(data=obj)
+        product = Saving.objects.get(pk=obj.get('fin_prdt_cd'))
+        if SavingOption.objects.filter(product=obj.get('fin_prdt_cd'), save_trm=obj.get('save_trm')).exists():
+            instance = SavingOption.objects.get(product=obj.get('fin_prdt_cd'), save_trm=obj.get('save_trm'))
+            options_serializer = SavingOptionSerializer(instance=instance, data=obj, partial=True)
+        else:
+            options_serializer = SavingOptionSerializer(data=obj)
         if options_serializer.is_valid(raise_exception=True):
             options_serializer.save(product=product)
     return Response({'message':'okay'})
