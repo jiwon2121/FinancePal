de29cdf0bfe0a22437c47ab726055080674ce290
suminjeff <suminjeff@gmail.com>
2023-11-21 11:18:38 +0900

    feat : 댓글 수정/삭제

--- final_pjt_back/articles/urls.py
+++ final_pjt_back/articles/urls.py
@@ -5,4 +5,5 @@ app_name = 'articles'
 urlpatterns = [
     path('<int:article_pk>/', views.article_detail),
     path('<str:board_type>/', views.article_list),
+    path('comment/<int:comment_pk>/', views.comment),
 ]
