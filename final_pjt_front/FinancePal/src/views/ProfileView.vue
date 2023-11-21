cfbd643e44de3dc4551a97c14486a2cb1e8a020e
jiwon <leeji7031@gmail.com>
2023-11-21 10:57:12 +0900

    fix: 회원가입시 firstname lastname이 추가되지 않는 버그 수정

--- final_pjt_front/FinancePal/src/views/ProfileView.vue
+++ final_pjt_front/FinancePal/src/views/ProfileView.vue
@@ -1,10 +1,31 @@
 <template>
   <div>
     <h1>ProfileView</h1>
+    {{ profile }}
+    <h3>{{ profile.nickname }}님의 프로필</h3>
+    <p>생일 : {{ profile.birth_date }}</p>
+    <p>email : {{ profile.email }}</p>
   </div>
 </template>
 
 <script setup>
+import axios from 'axios'
+import { ref } from 'vue'
+import { useRoute } from 'vue-router'
+
+const route = useRoute()
+const profile = ref(null)
+
+axios({
+  method: 'get',
+  url: `http://127.0.0.1:8000/profile/${route.params.username}/`
+})
+  .then(res => {
+    profile.value = res.data
+  })
+  .catch(err => {
+    console.log(err)
+  })
 
 </script>
 
