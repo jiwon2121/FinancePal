322f59af464dc979ed7b2b04a8c9df9e4fe25e89
suminjeff <suminjeff@gmail.com>
2023-11-23 17:41:44 +0900

    feat : 환율 단위 버튼 + 콤마 해결

--- final_pjt_back/products/views.py
+++ final_pjt_back/products/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, get_list_or_404
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.conf import settings
