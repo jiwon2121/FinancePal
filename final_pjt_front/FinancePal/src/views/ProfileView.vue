042ca7336311199e27c76ad9176108c165fffee7
suminjeff <suminjeff@gmail.com>
2023-11-23 17:09:53 +0900

    fix : 인증 라우터가드

--- final_pjt_front/FinancePal/src/views/ProfileView.vue
+++ final_pjt_front/FinancePal/src/views/ProfileView.vue
@@ -4,8 +4,8 @@
       <button class="mx-1 btn btn-secondary" @click="router.back()">뒤로 가기</button>
       <div>
         <button type="button" class="mx-1 btn btn-outline-dark" :class="{active:page===1}" @click="changePage(1)">활동내역</button>
-        <button type="button" class="mx-1 btn btn-outline-dark" :class="{active:page===2}" @click="changePage(2)">포트폴리오</button>
-        <button type="button" class="mx-1 btn btn-outline-dark" :class="{active:page===3}" @click="changePage(3)">상품 추천</button>
+        <button type="button" class="mx-1 btn btn-outline-dark" :class="{active:page===2}" v-if="route.params.username===accStore.userName" @click="changePage(2)">포트폴리오</button>
+        <button type="button" class="mx-1 btn btn-outline-dark" :class="{active:page===3}" v-if="route.params.username===accStore.userName" @click="changePage(3)">상품 추천</button>
       </div>
     </div>
     <Profile 
@@ -24,6 +24,7 @@
 import { ref, onMounted } from 'vue'
 import { useRoute, useRouter, onBeforeRouteUpdate } from 'vue-router'
 import { profileStore } from '@/stores/profileStore'
+import { accountStore } from '@/stores/accountStore'
 import Portfolio from '@/components/Portfolio.vue'
 import Profile from '@/components/Profile.vue'
 import Recommend from '@/components/Recommend.vue'
@@ -32,6 +33,7 @@ const page = ref(1)
 const store = profileStore()
 const route = useRoute()
 const router = useRouter()
+const accStore = accountStore()
 
 const changePage = function(num) {
   page.value = num
